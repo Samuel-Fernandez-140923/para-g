88ac63042322068699b5d63552bcfa5c0a3ffb38
Samuel-Fernandez-140923 <samueldukipkoko@gmail.com>
2024-11-27 00:30:56 -0500

    n1

---
+++ ConsoleApp1/Datos.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Datos
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; } 
+        public double Estatura { get; set; }
+
+        public override string ToString()
+        {
+            return "Nombre: " + Nombre + " Edad: " + Edad + " Tamaño: " + Estatura ;       
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int res;
+        int res2;
+        do
+        {
+            Console.WriteLine("Ingresa [1] para comezar el prgrama");
+            res=int.Parse(Console.ReadLine());
+         } while (res!=1);
+
+        int posicion = 0;
+
+        Datos datos = new Datos();
+        /////////////////////////////////////
+        string[] arregloNombre=new string[res];
+        int[] arregloEdad=new int[res]; 
+        double[] arregloTamaño = new double[res];
+       
+        do
+        {
+            Console.WriteLine("[1] Para ingresar datos");
+            Console.WriteLine("[2] Para finalizar");
+             res2=int.Parse(Console.ReadLine());
+            switch (res2) 
+            { 
+             case 1:
+                    Console.WriteLine("Ingrsa el nombre de la persona:");
+                    datos.Nombre = Console.ReadLine();
+                    arregloNombre[posicion] = datos.Nombre;
+                    Console.WriteLine("Ingrsa edad de la persona:");
+                    datos.Edad = int.Parse(Console.ReadLine());
+                    arregloEdad[posicion] = datos.Edad;
+                    Console.WriteLine("Ingrsa la estatura de la persona:");
+                    datos.Estatura = double.Parse(Console.ReadLine());
+                    arregloTamaño[posicion] = datos.Estatura;
+
+
+                    break;
+            
+            
+            }
+        }
+        while (res2!=2);
+    }
+}
